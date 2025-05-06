597ea4f408c9d3153f8cff4c8f40086fc94c5ae1
EduHamab <eduhamab@gmail.com>
2025-05-06 20:16:07 -0300

    Arquivos Iniciais de estudos

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com 3 numeros");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c  = sc.nextInt();
+
+        int maior = max(a, b,c);
+        verResultado(maior);
+
+        sc.close();
+    }
+
+    public static int max(int x, int y, int z){
+        int aux ;
+        if(x > y && x > z){
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+
+        return aux;
+    }
+
+    public static void verResultado(int valor){
+            System.out.printf("O maior é : %d", valor);
+    }
+
+}
+
+
---
+++ src/aula_003_POO.java
@@ -0,0 +1,8 @@
+public class aula_003_POO {
+    public static void main(String[] args) {
+
+        System.out.println("ola mundo");
+
+
+    }
+}
